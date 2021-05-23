687f96d24f5e15c67759c7e6d11a0f96bad16fb3
Joseungjin <jn5947@naver.com>
2021-05-23 23:11:48 +0900

    테스트

--- FinalProject_3/src/main/java/com/spring/finalproject3/joseungjin/finalproject3_seungjin.java
+++ FinalProject_3/src/main/java/com/spring/finalproject3/joseungjin/finalproject3_seungjin.java
@@ -1,5 +1,15 @@
 package com.spring.finalproject3.joseungjin;
 
-public class finalproject3_seungjin {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+public class finalproject3_seungjin {
+	
+	@RequestMapping(value="/index.sam")
+	public String test_test1() {
+		
+		return "home";
+	//  /WEB-INF/views/sample/test1.jsp
+	}
 }
