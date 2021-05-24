f62a21dcd2cdca8b9d7e749437b7b7feef1d87e7
Joseungjin <jn5947@naver.com>
2021-05-24 09:04:35 +0900

    조승진 메인 jsp 작업

--- FinalProject_3/src/main/java/com/spring/finalproject3/joseungjin/finalproject3_seungjin.java
+++ FinalProject_3/src/main/java/com/spring/finalproject3/joseungjin/finalproject3_seungjin.java
@@ -3,13 +3,17 @@ package com.spring.finalproject3.joseungjin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class finalproject3_seungjin {
 	
 	@RequestMapping(value="/index.sam")
-	public String test_test1() {
+	public ModelAndView login(ModelAndView mav) {
 		
-		return "home";
-	//  /WEB-INF/views/sample/test1.jsp
+		mav.setViewName("main/index.tiles1");
+		// /WEB-INF/views/tiles1/login/loginform.jsp 파일을 생성한다.
+		
+		return mav;
 	}
 }
